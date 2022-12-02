f4959495586987dba2930a5b5ebbce099580381e
Manuel <manuneva@ucm.es>
2022-12-02 10:49:25 +0100

    Ejercicio de exec

--- SO/pr3/execvp_ejer.c
+++ SO/pr3/execvp_ejer.c
@@ -2,9 +2,25 @@
 
 int main(int argc, char** argv){
     
-    execvp(argv[1], argv[2]);
-    
+    if(argc < 2){
+        printf("Low number of arguments\n");
+        printf("The executable must be followed by the command you want to execute\n");
+        return 1;
+    }
+
+    //aqui lo que hago es dejar claro que el nombre no lo quiero pero esque en la ultima posicion
+    //tengo que poner el NULL
+    char* arglist[argc-1+1];
+
+    for(int i=1;i<argc;i++)
+        arglist[i-1] = argv[i];
+
+    arglist[argc-1]=NULL;
+
+    execvp(argv[1],arglist);
+
     if(errno){
+        printf("An errno has been raised\n");
         perror(errno);
         return errno;
     }else
