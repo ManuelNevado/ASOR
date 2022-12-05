e302762af400ace10be5b873ff8208c69a8b9f3d
Manuel <manuneva@ucm.es>
2022-12-05 12:51:09 +0100

    primera sesion de lab

--- SO/pr4/ejer1.c
+++ SO/pr4/ejer1.c
@@ -11,11 +11,45 @@ Probar el funcionamiento con una sentencia similar a: ./ejercicio1 echo 12345 wc
 
 int main(int argc, char** argv){
     
-    //pipe(
+    if(argc!=5){
+        printf("Error en la entrada de argumentos\n");
+        return 1;
+    }
 
-    if(errno){
-        perror("Error en el pipe\n");
+    char* line1[3] = {argv[1],argv[2],NULL};
+    char* line2[3] = {argv[3], argv[4], NULL};
+
+    //pipes
+    //Se crea la tuberia sin nombre
+    int fd[2];
+
+    int paip = pipe(fd);
+
+    //Se crean los procesos
+
+    pid_t forked = fork();
+
+    if(forked == -1){
+        perror("Error en el fork(2)\n");
         return errno;
+    }
+
+    if(forked == 0){
+        //Child
+        close(fd[1]);
+        dup2(fd[0],0);//Entrada estandar = 0
+        close(fd[0]);
+        execvp(argv[3],line2);
+
+        
+    }else{
+        
+        //Parent
+        close(fd[0]);
+        dup2(fd[1],1);//1=stdout en file_descript
+        close(fd[1]);
+        execvp(argv[1], line1);
+        //Coger fallo de execvp
     }
 
 
---
+++ SO/pr4/ejer2.c
@@ -0,0 +1,85 @@
+/*
+ * Para la comunicación bi-direccional, es necesario crear dos tuberías, una para cada sentido: p_h y h_p. 
+ * Escribir un programa que implemente el mecanismo de sincronización de parada y espera:
+ * 
+ * El padre leerá de la entrada estándar (terminal) y enviará el mensaje al proceso hijo, 
+ * escribiéndolo en la tubería p_h. 
+ * Entonces permanecerá bloqueado esperando la confirmación por parte del hijo en la otra tubería, h_p.
+ * El hijo leerá de la tubería p_h, escribirá el mensaje por la salida estándar y esperará 1 segundo. 
+ * Entonces, enviará el carácter ‘l’ al proceso padre, escribiéndolo en la tubería h_p, 
+ * para indicar que está listo. 
+ * Después de 10 mensajes enviará el carácter ‘q’ para indicar al padre que finalice.
+ */
+
+#include "cabeceras.h"
+#define MAX_BUFF 200
+
+void raise_error(){
+    perror("ERROR\n");
+}
+
+int main(int argc, char** argv){
+
+    //Prepare the pipes
+    int p_h[2];
+    int h_p[2];
+
+    pipe(p_h);
+    pipe(h_p);
+
+    if(errno){
+        perror("ERROR\n");
+        return errno;
+
+    pid_t forked = fork();
+    if(errno)
+        return errno;//Error en fork
+
+    if(forked == 0){
+        //Child
+        //Prepare the buffer to read and write    
+        char buffer[MAX_BUFF];
+        int len;
+        //line = getline(&buffer,&len, stdin);
+        
+        int i=0;
+        while(i<10){
+            //Escribir
+            printf("Desde el hijo: Escribe lo que quieras enviar -> ");
+            int length = getline(buffer, &len, stdin);
+            write(h_p[1],buffer,len);
+            if (errno)
+                raise_error();
+            //Leer
+            int size = read(p_h[0],buffer, MAX_BUFF);
+            if(errno)
+                raise_error();
+            printf("Desde el hijo: Mensaje recibido -> %s\n",buffer);
+            //i++
+            i++;
+        }
+    }else{
+        //Parent
+        //Prepare the buffer to read and write    
+        int i=0;
+        while(i<10){
+            char buffer[MAX_BUFF];
+            int line;
+            unsigned int lenght;
+            
+            //Lectura
+            int size = read(h_p[0],buffer, MAX_BUFF);
+            if(errno)
+                raise_error();
+            printf("Desde el padre: Mensaje recibido -> %s\n",buffer);
+            //Escritura
+            printf("Desde el padre: Escribe lo que quieras enviar -> ");
+            line = getline(buffer, &len, stdin);
+            write(p_h[1],buffer,len);
+            if (errno)
+                raise_error();
+            i++;
+        } 
+
+     }
+}
